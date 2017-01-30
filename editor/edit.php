91c5123f5c68cc8cede053731e42af7102f21703
Noah Ratcliff <me@nratcliff.com>
2017-01-30 12:24:47 -0500

    add user info to edit.php

--- editor/edit.php
+++ editor/edit.php
@@ -4,6 +4,14 @@
 		header("Location: ../login/");
 	}
 	
+	// get user information (for filling out form)
+	$user = $_SESSION["user"];
+	$dbh = new PDO("sqlite:../../../db/users.sql") or die("Could not establish a database connection.");
+	$sth = $dbh->prepare("SELECT email, firstname, lastname, organization FROM users WHERE username = :username");
+    $sth->execute(array(":username"=>$user));
+	if(!$res = $sth->fetch())
+		die("Failed to load user information");
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,6 +40,15 @@
 			else
 				document.forms["password"].submit();
 		}
+
+		function submitInfo(){
+			if(document.forms["info"].checkValidity()){
+				document.forms["info"].submit();
+			}
+			else {
+				alert("All information fields are required.");
+			}
+		}
 	</script>
 </head>
 <body>
@@ -42,22 +59,14 @@
     			<fieldset>
     				<legend>Email</legend>
 	    			Current:
-	    			<?php 
-	    				$user = $_SESSION["user"];
-	    				$dbh = new PDO('sqlite:../../../db/users.sql') or die ("cannot open");
-						//$result = $db->query("SELECT email FROM users WHERE username = '$user'");
-                        $sth = $dbh->prepare("SELECT email FROM users WHERE username = :username");
-                        $sth->execute(array(":username"=>$user));
-		    			if($res = $sth->fetch())
-	    					echo $res["email"]; 
-	    			?>
+	    			<?php echo $res["email"]; ?>
 					<hr>
 	    			New:
 	    			<input type="email" name="email" required />
 	    			<a href="#" onclick="submitEmail();" class="menuButton">Change Email</a>
     			</fieldset>
     		</form>
-    		<form name="password" action="changePass.php" method="POST">
+    		<form name="password" action="changePass.php" method="POST" style="padding-bottom:25px;">
     			<fieldset>
     				<legend>Password</legend>
 	    			Old:
@@ -69,6 +78,18 @@
 	    			<a href="#" onclick="submitPass();" class="menuButton">Change Password</a>
 	    		</fieldset>
     		</form>
+			<form name="info" action="changeInfo.php" method="POST">
+				<fieldset>
+					<legend>Information</legend>
+					First Name:
+					<input type="text" name="firstname" value="<?php echo $res["firstname"]; ?>" required >
+					Last Name:
+					<input type="text" name="lastname" value="<?php echo $res["lastname"]; ?>" required >
+					Organization:
+					<input type="text" name="organization" value="<?php echo $res["organization"]; ?>" required >
+					<a href="#" onclick="submitInfo();" class="menuButton">Update</a>
+				</fieldset>
+			</form>
 			<a href="./account.php" class="menuButton">Back</a>
 		</div>
 		<img class="logo" src="../img/nsflogo.png" />
