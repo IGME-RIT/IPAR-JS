36b47a62d25eb303527cffc584be93423625b88c
Noah Ratcliff <me@nratcliff.com>
2017-01-30 14:35:16 -0500

    get user metadata on account created

--- createDatabase.php
+++ createDatabase.php
@@ -3,10 +3,13 @@
    $usersSchema = "CREATE TABLE users (username CHAR(255) PRIMARY KEY NOT NULL, email CHAR(255) NOT NULL UNIQUE, password CHAR(255) NOT NULL, curKey CHAR(255) UNIQUE, active INT, firstname CHAR(255) NOT NULL DEFAULT '', lastname CHAR(255) NOT NULL DEFAULT '', organization CHAR(255) NOT NULL DEFAULT '')";
    $imagesSchema = "CREATE TABLE images (file CHAR(255) PRIMARY KEY NOT NULL, name TEXT, username CHAR(255))";
    $resourcesSchema = "CREATE TABLE resources (file CHAR(255) PRIMARY KEY NOT NULL, name TEXT, username CHAR(255))";
+   $usermetadataSchema = "CREATE TABLE usermetadata (username CHAR(255) PRIMARY KEY NOT NULL, ip INTEGER NOT NULL, useragent TEXT NOT NULL, datetime INTEGER NOT NULL);";
    if($dbh){
+       // create tables
        $dbh->query($usersSchema);
        $dbh->query($imagesSchema);
        $dbh->query($resourcesSchema);
+       $dbh->query($usermetadataSchema);
 	   echo "CREATED";
    }
    else{
--- login/new_user.php
+++ login/new_user.php
@@ -98,7 +98,7 @@
    		$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
         
         // create user record in users
-        $insStatement = $db->prepare("INSERT INTO users VALUES (:username, :email, :password, :curKey, 0, :firstname, :lastname, :organization)");
+        $sth = $db->prepare("INSERT INTO users VALUES (:username, :email, :password, :curKey, 0, :firstname, :lastname, :organization)");
         
         // prepare parameters set
         $params = array(":username" => $user, 
@@ -109,13 +109,27 @@
                         ":lastname" => $lastname,
                         ":organization" => $organization);
         
-        $success = $insStatement->execute($params);
+        $success = $sth->execute($params);
         if(!$success){
             // print_r($db->errorInfo());
             
             // TODO: put some error logging here -ntr
             die("<br><br>Failed to create account. Please contact the site administrator.");
         }
+
+        // collect user metadata
+        $date = new DateTime();
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $useragent = $_SERVER['HTTP_USER_AGENT'];
+
+        // insert user metadata 
+        $sth = $db->prepare("INSERT INTO usermetadata VALUES (:username, :ip, :useragent, :date)");
+        $sth->execute(array(
+            ":username"=>$user,
+            ":ip"=>ip2long($ip),
+            ":useragent"=>$useragent,
+            ":date"=>$date->getTimestamp()
+        ));
         
         // get appliction URL 
         // TODO: this could probably be stored in a config table -ntr
@@ -130,6 +144,11 @@
    		$msg = "Thank you for creating an IPAR Editor Account! You can use this account to create IPAR cases and to manage both the images and resources for them! To activate your account please use the following link:\n\nhttp://$path/activate.php?key=$key&";
    		mail($_POST['email'],'Account Activation',wordwrap($msg,70),"From: IPAR Editor <yin.pan@rit.edu>");
         
+        // send new user email to admin
+        $msg = "A new IPAR account has just been created:\nUsername: ".$user."\nEmail: ".$email."\nName: ".$firstnmae." ".$lastname."\nOrganization: ".$organization."\nIP: ".$ip."Useragent: ".$useragent;
+//        mail('yin.pan@rit.edu', 'New IPAR Account', $msg, "From: IPAR (Noreply) <no-reply@rit.edu>");
+        mail('ntr5008@rit.edu', 'New IPAR Account', $msg, "From: IPAR (Noreply) <no-reply@rit.edu>"); //debug email, uncomment above for prod.
+
         // redirect to message screen
    		header("Location: ./message.html?message=Your account has been created! You will be been emailed a confirmation email shortly. Please use it to confirm your email and unlock your account for use.&");
    	}
