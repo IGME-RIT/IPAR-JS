712e4ab8cc017dabb1ac39c910c4f06e5cad6611
Noah Ratcliff <me@nratcliff.com>
2017-01-30 16:54:16 -0500

    add default role to new users

--- login/new_user.php
+++ login/new_user.php
@@ -130,6 +130,10 @@
             ":useragent"=>$useragent,
             ":date"=>$date->getTimestamp()
         ));
+
+        // add default user role (editor)
+        $sth = $db->prepare("INSERT INTO users_roles (username, roleid) SELECT :username as username, roles.rowid as roleid FROM roles WHERE roles.name=:role");
+        $sth->execute(array(":username"=>$user, ":role"=>"editor"));
         
         // get appliction URL 
         // TODO: this could probably be stored in a config table -ntr
@@ -145,7 +149,7 @@
    		mail($_POST['email'],'Account Activation',wordwrap($msg,70),"From: IPAR Editor <yin.pan@rit.edu>");
         
         // send new user email to admin
-        $msg = "A new IPAR account has just been created:\nUsername: ".$user."\nEmail: ".$email."\nName: ".$firstnmae." ".$lastname."\nOrganization: ".$organization."\nIP: ".$ip."Useragent: ".$useragent;
+        $msg = "A new IPAR account has just been created:\nUsername: ".$user."\nEmail: ".$email."\nName: ".$firstname." ".$lastname."\nOrganization: ".$organization."\nIP: ".$ip."Useragent: ".$useragent;
 //        mail('yin.pan@rit.edu', 'New IPAR Account', $msg, "From: IPAR (Noreply) <no-reply@rit.edu>");
         mail('ntr5008@rit.edu', 'New IPAR Account', $msg, "From: IPAR (Noreply) <no-reply@rit.edu>"); //debug email, uncomment above for prod.
 
