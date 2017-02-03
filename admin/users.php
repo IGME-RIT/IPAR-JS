f9af7e2fe2b69f2808c0092b96c701bd0f45e444
Noah Ratcliff <me@nratcliff.com>
2017-02-03 12:53:38 -0500

    toggle user role in users.php

--- admin/users.php
+++ admin/users.php
@@ -8,10 +8,15 @@ if(!$res = $dbh->query($query)){
     die("Failed to read from database.");
 }
 
-// prepare roles statement
-$rolesth = $dbh->prepare("SELECT roles.name FROM users_roles 
-                            INNER JOIN roles ON users_roles.roleid = roles.rowid 
-                            WHERE users_roles.username = :username")
+// prepare user roles statement
+//$rolesth = $dbh->prepare("SELECT roles.name FROM users_roles 
+//                            INNER JOIN roles ON users_roles.roleid = roles.rowid 
+//                            WHERE users_roles.username = :username");
+
+$rolesth =  $dbh->prepare("SELECT name, id, (username IS NOT NULL) AS hasrole FROM (
+                            SELECT users_roles.username, roles.name, roles.rowid as id FROM roles
+                            LEFT JOIN users_roles 
+                            ON (users_roles.username=:username And roles.rowid = users_roles.roleid));");
 
 ?>
 
@@ -23,6 +28,29 @@ $rolesth = $dbh->prepare("SELECT roles.name FROM users_roles
         <link href="https://fonts.googleapis.com/css?family=Quicksand:700" rel="stylesheet" type="text/css">
         <link rel="stylesheet" type="text/css" href="../css/styles.css">
         <link rel="stylesheet" type="text/css" href="../css/menuStyle.css">
+        <script type="text/javascript">
+            function roleCheckbox(element){
+                // make an ajax call to set the user's roles
+                
+                var roleid = element.getAttribute("data-roleid");
+                var user = element.getAttribute("data-user");
+                var value = element.checked ? 1 : 0;
+
+                console.log("Setting role '" + element.value + " (" + roleid + ")' as " + value + " for user " + user); 
+                
+                var request = new XMLHttpRequest();
+                request.open("POST", "setUserRole.php");
+                /*request.onreadystatechange = function() {
+                    if(request.readyState === XMLHttpRequest.DONE) {
+                        alert(request.responseText);
+                    }
+                }*/
+                //TODO: alert on errors
+                request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                request.send('user='+user+"&roleid="+roleid+"&value="+value);
+
+            }
+        </script>
     </head>
     <body>
         <section class="menu">
@@ -58,7 +86,15 @@ $rolesth = $dbh->prepare("SELECT roles.name FROM users_roles
                             <td><?php echo $row['firstname']; ?></td>
                             <td><?php echo $row['lastname']; ?></td>
                             <td><?php echo $row['organization'] ?></td>
-                            <td><?php while($rolerow = $rolesth->fetch()) { echo $rolerow['name'].","; } ?></td>
+                            <td>
+                                <?php 
+                                while($rolerow = $rolesth->fetch()) { 
+                                   ?>
+                                   <label><input type="checkbox" value="<?php echo $rolerow['name']; ?>" <?php if($rolerow['hasrole'] == 1) {?> checked="checked" <?php } ?> data-user="<?php echo $row['username'];?>" data-roleid="<?php echo $rolerow['id']; ?>" onchange="roleCheckbox(this);"> <?php echo $rolerow['name']; ?></label>
+                                   <?php 
+                                } 
+                                ?>
+                            </td>
                             <td><?php echo $ip ?></td>
                             <td><?php echo $row['useragent'] ?></td>
                             <td><?php echo $date ?></td>
