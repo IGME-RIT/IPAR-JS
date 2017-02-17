8cbb5acd896fab99874b6bba93caac3bacb83eeb
Noah Ratcliff <me@nratcliff.com>
2017-02-17 12:21:46 -0500

    update relative path

--- ipar/login/loginCheck.php
+++ ipar/login/loginCheck.php
@@ -1,29 +1,32 @@
 <?php
-   session_start();
-   if($_POST){
-	   $dbh = new PDO('sqlite:../../../db/users.sql') or die ("cannot open");
-	   $user = strtolower($_POST['username']);
-	   if($user && $_POST['password'] && $_POST['password']!="" && preg_match('/^[a-z0-9_]+$/', $user)==1){
-		   //$result = $db->query("SELECT password FROM users WHERE username = '$user'");
-           $sth = $dbh->prepare("SELECT password FROM users WHERE username = :username");
-           $sth->execute(array(":username"=>$user));
-		   if($res = $sth->fetch()){
-		   	if(password_verify($_POST['password'] , $res['password'])){
-		   		$_SESSION["user"] = $user;
-		   		header("Location: ../editor/");
-		   		exit();
-		   	}
-		   }
-	   }
-   	   echo "<script type='text/javascript'>
+    $rootPath = realpath(dirname(__FILE__)."/../../../");
+    echo $rootPath;
+
+    session_start();
+    if($_POST){
+        $dbh = new PDO('sqlite:'.$rootPath.'/db/users.sql') or die ("cannot open");
+        $user = strtolower($_POST['username']);
+        if($user && $_POST['password'] && $_POST['password']!="" && preg_match('/^[a-z0-9_]+$/', $user)==1){
+            //$result = $db->query("SELECT password FROM users WHERE username = '$user'");
+            $sth = $dbh->prepare("SELECT password FROM users WHERE username = :username");
+            $sth->execute(array(":username"=>$user));
+            if($res = $sth->fetch()){
+                if(password_verify($_POST['password'] , $res['password'])){
+                    $_SESSION["user"] = $user;
+                    header("Location: ../editor/");
+                    exit();
+                }
+            }
+        }
+        echo "<script type='text/javascript'>
 			   alert('Invaild username or password!');
 			   window.location.href = './login.php?username=$user&';
 		   </script>";
-   	   exit();
-   }
-   echo "<script type='text/javascript'>
+        exit();
+    }
+    echo "<script type='text/javascript'>
 		   alert('Invaild username or password!');
 		   window.location.href = './login.php';
-	   </script>";
-   exit();
+        </script>";
+    exit();
 ?>
