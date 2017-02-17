8fe8a694e32d6af97fb3a666a5f9c66126efd1fd
Noah Ratcliff <me@nratcliff.com>
2017-02-17 15:39:06 -0500

    use user_auth.php in checkadmin

--- ipar/login/checkAdmin.php
+++ ipar/login/checkAdmin.php
@@ -1,28 +1,14 @@
 <?php
-session_start();
+include $_SERVER['DOCUMENT_ROOT']."/assets/php/user_auth.php"; // sets $loggedIn, $dbh and $_SESSION['user_roles']
+
 // direct to login screen if user is not logged in
-if(!$_SESSION || !$_SESSION["user"]){
+if(!loggedIn){
     header("Location: ../login/login.php");
     exit();
 }
 
-$user = $_SESSION["user"];
-
 // check if user has admin rights
-$authenticated = false;
-$dbh = new PDO("sqlite:../../../db/users.sql") or die ("Could not establish a database connection.");
-$sth = $dbh->prepare("SELECT roles.name FROM users_roles JOIN roles ON roles.rowid = users_roles.roleid WHERE users_roles.username = :username");
-if($sth->execute(array(":username"=> $user))){
-    $rows = $sth->fetchAll();
-    foreach($rows as $row){
-        if($row["name"] == "admin"){
-            // authenticated
-            $authenticated = true;
-        }
-    }
-}
-
-if(!$authenticated){
+if(!in_array('admin', $_SESSION['user_roles'])){
     header("Location: ../login/message.html?message=You are not authorized to view this page.&");
     exit();
 }
