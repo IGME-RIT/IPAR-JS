c439d6ad2433507af0aa4343caf3ce617d32e7b9
Noah Ratcliff <me@nratcliff.com>
2017-02-20 16:51:35 -0500

    update admin panel to match mockup

--- ipar/admin/index.php
+++ ipar/admin/index.php
@@ -1,18 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>IPAR</title>
-        <link href="https://fonts.google.com/css?family=Quicksand:700" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" type="text/css" href="../css/menuStyle.css">
-    </head>
-    <body>
-        <section class="menu">
-            <div>
-                <h1>Admin Panel</h1>
-                <a href="./users.php" class="menuButton">Users</a>
-                <a href="../" class="menuButton">Back</a>
-            </div>
-        </section>
-    </body>
+	<head>
+		<title>IPAR - Admin Panel</title>
+		<?php include $_SERVER['DOCUMENT_ROOT']."/assets/html/head.php"; ?>
+	</head>
+	<body>
+		<?php include $_SERVER['DOCUMENT_ROOT']."/assets/html/navbar.php"; ?>
+		<div class="jumbotron">
+			<div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-vcenter">
+                        <h1 class="uline">Admin Panel</h1>
+                    </div>
+                </div>
+                <ul class="panel-buttons">
+                    <li>
+                        <a href="users.php" class="btn-tile">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            <span class="name">Users</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" class="btn-tile">
+                            <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+                            <span class="name">Home</span>
+                        </a>
+                    </li>
+                </ul>
+			</div>
+		</div>
+		<?php include $_SERVER['DOCUMENT_ROOT']."/assets/html/footer.php"; ?>
+	</body>
 </html>
