219fd815bd52b13f3f6f9dde200a09f97a28c680
Noah Ratcliff <me@nratcliff.com>
2017-02-23 13:27:47 -0500

    update about page

--- about.php
+++ about.php
@@ -6,62 +6,135 @@
 	</head>
 	<body>
 		<?php include("assets/html/navbar.php"); ?>
-		<div style="height:65px"></div>
-		<div class="container">
-			
-			<div class="jumbotron">
-				<h2><strong>About US</strong></h2>
-				<p>Funded in part by the National Science Foundation under Award DUE-1400567, RIT faculty have been working
-					with Onondaga Community College and Corning Community College to develop a sequence
-					of entertaining, engaging, and educational forensic games, suitable for first year
-					students in college. Following narrative and/or storylines of the game via interactive
-					dialogs and visualized abstract concepts, students are motivated and engaged to obtain
-				the necessary knowledge. </p>
-				<p>The forensic game framework is modular and flexible and can easily
-					be applied to other areas of STEM programs. A GUI-based game creator
-					is developed to assist easy creation of new games for different
-					domains.
-				</p>
-				<h3><strong>Involved Members of the Project:</strong></h3>
+		<div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-vcenter">
+                        <h1 style="margin-bottom: 0;">About IPAR</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="jumbotron light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="uline heavy">What is IPAR?</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <p>IPAR (Imaging, Preserving, analyzing and reporting) is a narrative-based detective-themed adventure game. In the game, the player assumes the role of an investigator: collecting evidence, answering questions, and drawing conclusions as part of a simulated investigation. Players are guided through a series of scripted steps, allowing them to gain practical experience and draw their own conclusions by answering subject related questions.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="yt-embed">
+                            <iframe class="yt-player" src="https://www.youtube.com/embed/Oq8GtXyUz1A" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h2 class="uline heavy">IPAR Editor</h2>
+                        <p>Developed alongside the game is an editor that can be used to generate new cases. Everything from subject matter to graphical elements to story can be set by the user to create an entertaining educational experience, and an reader that allows instructors to view the reports submitted by students for grading.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="yt-embed">
+                            <iframe class="yt-player" src="https://www.youtube.com/embed/i9FSdeNNRk8" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="jumbotron light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="uline heavy">Project Goals</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <ul>
+                            <li><p>Develop a sequence of fun, entertaining, yet educational game-based digital forensics modules to enhance digital forensics curricula and help create a pathway for students in forensics programs from two-year colleges to four-year institutes (Curricular Goal). 
+                                These innovative modules will be built into games in a real computing environment that has direct access to latest forensics software. In addition, the games’ integrated graphical visualizations will help entry-level students understand intangible and inaccessible abstract concepts, such as deleted/hidden/encrypted/over-written digital evidence.</p></li>
+                            <li><p>Develop faculty expertise, targeting two-year college faculty, in digital forensics and security field through collaborations within and between partnering institutions (Faculty Goal).</p></li>
+                            <li><p>Disseminate the game-based forensics course modules to community colleges and universities through workshops, tutorials/panels, websites such as the ATE Centers, published papers and conference presentations (Dissemination Goal).</p></li>
+                            <li><p>Assess the effectiveness of the games-based course modules via a comprehensive evaluation plan utilizing an external evaluator and a variety of data sources (Assessment Goal). The Assessment and Evaluation (A&amp;E) plan covers the effectiveness of each module in achieving its outcomes for students and faculty using a pre-post test comparison. The four major project goals listed above will be assessed through a well-crafted A&amp;E plan.</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<div class="jumbotron">
+			<div class="container">
 				<div class="row">
-					<div class="col-xs-12">
-						<span class="label label-primary">Yin Pan</span>
-						<span class="label label-primary">Sumita Mishra</span>
-						<span class="label label-primary">David Schwartz</span>
-						<span class="label label-primary">Ryan McGlinn</span>
-						<span class="label label-primary">Kelly Murdoch-Kitt</span>
-						
+					<div class="col-md-12">
+						<h2 class="uline heavy">Credits</h2>
 					</div>
 				</div>
-				<div class="row top-buffer">
-					<div class="col-xs-12">
-						<span class="label label-primary">Sarvagya Mishra</span>
-						<span class="label label-primary">Pamela McCarthy</span>
-						<span class="label label-primary">Alicia McNett</span>
-						<span class="label label-primary">Timothy Stedman</span>
-						
-						
-					</div>
-				</div>
-				<div class="row top-buffer">
-					<div class="col-xs-12">
-						<span class="label label-primary">Nicholas Fealey (Rochester Institute of Technology)</span>
-					</div>
-				</div>
-				<div class="row top-buffer">
-					<div class="col-xs-12">
-						<span class="label label-primary">Noah Ratcliff (Rochester Institute of Technology)</span>
-					</div>
-				</div>
-				<div class="row top-buffer">
-					<div class="col-xs-12">
-						<span class="label label-primary">Joseph DeLeone (Corning Community College)</span>
-					</div>
-				</div>
-				<div class="row top-buffer">
-					<div class="col-xs-12">
-						<span class="label label-primary">Michael Heise (Onondaga Community College)</span>
-						
+				<div class="row">
+					<div class="col-md-12">
+
+    	    			<p>Funded in part by the National Science Foundation under Award DUE-1400567, RIT faculty have been working
+    	    				with Onondaga Community College and Corning Community College to develop a sequence
+    	    				of entertaining, engaging, and educational forensic games, suitable for first year
+    	    				students in college. Following narrative and/or storylines of the game via interactive
+    	    				dialogs and visualized abstract concepts, students are motivated and engaged to obtain
+    	    			the necessary knowledge. </p>
+    	    			<p>The forensic game framework is modular and flexible and can easily
+    	    				be applied to other areas of STEM programs. A GUI-based game creator
+    	    				is developed to assist easy creation of new games for different
+    	    				domains.
+    	    			</p>
+    	    			<h3><strong>Involved Members of the Project:</strong></h3>
+    	    			<div class="row">
+    	    				<div class="col-xs-12">
+    	    					<span class="label label-primary">Yin Pan</span>
+    	    					<span class="label label-primary">Sumita Mishra</span>
+    	    					<span class="label label-primary">David Schwartz</span>
+    	    					<span class="label label-primary">Ryan McGlinn</span>
+    	    					<span class="label label-primary">Kelly Murdoch-Kitt</span>
+    	    					
+    	    				</div>
+    	    			</div>
+    	    			<div class="row top-buffer">
+    	    				<div class="col-xs-12">
+    	    					<span class="label label-primary">Sarvagya Mishra</span>
+    	    					<span class="label label-primary">Pamela McCarthy</span>
+    	    					<span class="label label-primary">Alicia McNett</span>
+    	    					<span class="label label-primary">Timothy Stedman</span>
+    	    					
+    	    					
+    	    				</div>
+    	    			</div>
+    	    			<div class="row top-buffer">
+    	    				<div class="col-xs-12">
+    	    					<span class="label label-primary">Nicholas Fealey (Rochester Institute of Technology)</span>
+    	    				</div>
+    	    			</div>
+    	    			<div class="row top-buffer">
+    	    				<div class="col-xs-12">
+    	    					<span class="label label-primary">Noah Ratcliff (Rochester Institute of Technology)</span>
+    	    				</div>
+    	    			</div>
+    	    			<div class="row top-buffer">
+    	    				<div class="col-xs-12">
+    	    					<span class="label label-primary">Joseph DeLeone (Corning Community College)</span>
+    	    				</div>
+    	    			</div>
+    	    			<div class="row top-buffer">
+    	    				<div class="col-xs-12">
+    	    					<span class="label label-primary">Michael Heise (Onondaga Community College)</span>
+    	    					
+    	    				</div>
+    	    			</div>
 					</div>
 				</div>
 			</div>
