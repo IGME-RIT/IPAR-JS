1b21ee4a6685983401afcdb851516fb64e793b08
Noah Ratcliff <me@nratcliff.com>
2017-03-20 12:01:37 -0400

    update resetPass style

--- ipar/login/resetPass.php
+++ ipar/login/resetPass.php
@@ -12,7 +12,8 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>IPAR</title>
+	<title>IPAR - Reset Password</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/head.php'; ?>
 	<link href='https://fonts.googleapis.com/css?family=Quicksand:700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="../css/menuStyle.css">
 	<script type='text/javascript'>
@@ -30,19 +31,43 @@
 	</script>
 </head>
 <body>
-    <section class="menu">
-    	<div>
-    		<h1>Reset Password</h1>
-    		<form name="reset" action="changePass.php" method="POST">
-				New Password: <input type="password" name="password" required />
-				<hr>
-				Confirm Password: <input type="password" name="password2" required />
-				<input type="text" name="key" value="<?php echo $key ?>" style="display:none;" />
-				<a onclick="submit();" href="#" class="menuButton">Reset</a>
-				<a href="./" class="menuButton">Back</a>
-			</form>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/navbar.php'; ?>
+    <div class="jumbotron">
+		<div class="container">
+        		<h1 class="uline">Reset Password</h1>
+        		<form name="reset" action="changePass.php" method="POST">
+			    	<div class="row">
+						<div class="col-xs-12">
+							New Password: <input type="password" name="password" required />
+						</div>
+					</div>
+					<div class="row" style="margin-top: 5px;">
+						<div class="col-xs-12">
+			    			Confirm Password: <input type="password" name="password2" required />
+						</div>
+					</div>
+					<input type="hidden" name="key" value="<?php echo $key ?>"/>
+					<div class="row" style="margin-top: 10px;">
+					    <ul class="panel-buttons col border">
+			            	<div class="col-xs-12 col-md-4">
+					    		<li><a href="./" class="btn-tile horiz">
+									<span class="glyphicon glyphicon-arrow-left"></span>
+									<span class="name">Back</span>
+								</a></li>
+					    	</div>
+					        <div class="col-xs-12 col-md-8">
+					    		<li><a onclick="submit();" href="#" class="btn-tile horiz">
+									<span class="glyphicon glyphicon-refresh"></span>
+									<span class="name">Change Password</span>
+								</a></li>
+					    	</div>
+					    </ul>
+					</div>
+			    </form>
+			</div>
 		</div>
-		<img class="logo" src="../img/nsflogo.png" />
-    </section>
+		</div>
+    </div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/footer.php'; ?>
 </body>
 </html>
