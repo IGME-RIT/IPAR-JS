99143f81cb578e3b4de096f0a11598373018f932
Noah Ratcliff <me@nratcliff.com>
2017-03-21 17:05:38 -0400

    update recoverPass style

--- ipar/login/recoverPass.php
+++ ipar/login/recoverPass.php
@@ -9,21 +9,43 @@
 <head>
 	<meta charset="utf-8" />
 	<title>IPAR</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/head.php'; ?>
 	<link href='https://fonts.googleapis.com/css?family=Quicksand:700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="../css/menuStyle.css">
 </head>
 <body>
-    <section class="menu">
-    	<div>
-    		<h1>Lost Password</h1>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/navbar.php'; ?>
+    <div class="jumbotron">
+    	<div class="container">
+			<div class="row">
+    			<div class="col-xs-12"><h1 class="uline">Lost Password</h1></div>
+			</div>
     		<form name="recoverPass" action="recoverPassCheck.php" method="POST">
-    			Enter the username of the account:<br>
-				<input type="text" name="username" />
-				<a onclick="document.forms['recoverPass'].submit();" href="#" class="menuButton">Recover</a>
-				<a href="./login.php" class="menuButton">Back</a>
+    			<div class="row"><div class="col-xs-12">Enter the username of the account:</div></div>
+				<div class="row"><div class="col-xs-12"><input type="text" name="username" /></div></div>
+				<div class="row">
+					<ul class="panel-buttons col border" style="margin-top: 10px">
+						<div class="col-md-4 col-xs-12">
+							<li>
+								<a href="./login.php" class="btn-tile horiz">
+									<span class="glyphicon glyphicon-arrow-left"></span>
+									<span class="name">Back</span>
+								</a>
+							</li>
+						</div>
+						<div class="col-md-8 col-xs-12">
+							<li>
+								<a onclick="document.forms['recoverPass'].submit();" href="#" class="btn-tile horiz">
+									<span class="glyphicon glyphicon-refresh"></span>
+									<span class="name">Recover</span>
+								</a>
+							</li>
+						</div>
+					</ul>
+				</div>
 			</form>
 		</div>
-		<img class="logo" src="../img/nsflogo.png" />
-    </section>
+    </div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/footer.php'; ?>
 </body>
 </html>
