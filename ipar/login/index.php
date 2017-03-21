a6a9dcbd9d72ab170233227499e3d7fba3e2c5d2
Noah Ratcliff <me@nratcliff.com>
2017-03-21 15:10:49 -0400

    update ligin index style

--- ipar/login/index.php
+++ ipar/login/index.php
@@ -20,23 +20,23 @@
         	<div class="row">
 				<div class="col-xs-12">
             		<h1 class="uline">IPAR Login</h1>
-					<ul class="panel-buttons col">
+					<ul class="panel-buttons ">
     			    	<li>
-							<a href="./login.php" class="btn-tile horiz">
-								<span class="glyphicon glyphicon-%gly%"></span>
+							<a href="./login.php" class="btn-tile ">
+								<span class="glyphicon glyphicon-log-in"></span>
 								<span class="name">Login</span>
 							</a>
 						</li>
     			    	<li>
-							<a href="./signup.php" class="btn-tile horiz">
-								<span class="glyphicon glyphicon-%gly%"></span>
+							<a href="./signup.php" class="btn-tile ">
+								<span class="glyphicon glyphicon-plus"></span>
 								<span class="name">Sign Up</span>
 							</a>
 						</li>
     			    	<li>
-							<a href="../" class="btn-tile horiz">
-								<span class="glyphicon glyphicon-%gly%"></span>
-								<span class="name">Back to Main Menu</span>
+							<a href="../" class="btn-tile ">
+								<span class="glyphicon glyphicon-home"></span>
+								<span class="name">Home</span>
 							</a>
 						</li>
 					</ul>
