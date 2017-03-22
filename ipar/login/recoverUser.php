cbe01282d56660d9c23d855fdfbece502a112984
Noah Ratcliff <me@nratcliff.com>
2017-03-22 12:00:51 -0400

    update recoverUser style

--- ipar/login/recoverUser.php
+++ ipar/login/recoverUser.php
@@ -8,22 +8,54 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>IPAR</title>
+	<title>IPAR - Recover User</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/head.php' ?>
 	<link href='https://fonts.googleapis.com/css?family=Quicksand:700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="../css/menuStyle.css">
 </head>
 <body>
-    <section class="menu">
-    	<div>
-    		<h1>Lost Username</h1>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/navbar.php' ?>
+    <div class="jumbotron">
+    	<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+    				<h1 class="uline">Lost Username</h1>
+				</div>
+			</div>
     		<form name="recoverUser" action="recoverUserCheck.php" method="POST">
-    			Enter the email address associated with the account:<br>
-				<input type="email" name="email" />
-				<a onclick="document.forms['recoverUser'].submit();" href="#" class="menuButton">Recover</a>
-				<a href="./login.php" class="menuButton">Back</a>
+				<div class="row">
+					<div class="col-xs-12">
+   			 			Enter the email address associated with the account:
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<input type="email" name="email" />
+					</div>
+				</div>
+				<div class="row">
+					<ul class="panel-buttons col border" style="margin-top: 10px">
+						<div class="col-md-4 col-xs-12">
+							<li>
+								<a href="./login.php" class="btn-tile horiz">
+									<span class="glyphicon glyphicon-arrow-left"></span>
+									<span class="name">Back</span>
+								</a>
+							</li>
+						</div>
+						<div class="col-md-8 col-xs-12">
+							<li>
+								<a onclick="document.forms['recoverUser'].submit();" href="#" class="btn-tile horiz">
+									<span class="glyphicon glyphicon-refresh"></span>
+									Recover
+								</a>
+							</li>
+						</div>
+					</ul>
+				</div>
 			</form>
 		</div>
-		<img class="logo" src="../img/nsflogo.png" />
-    </section>
+    </div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/html/footer.php' ?>
 </body>
 </html>
