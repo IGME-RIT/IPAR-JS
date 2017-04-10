e0b177f9ffdaa1e346b7b348434a0857ef9448d6
Noah Ratcliff <me@nratcliff.com>
2017-04-10 15:06:13 -0400

    remove help button from game index

--- ipar/game/index.php
+++ ipar/game/index.php
@@ -84,12 +84,12 @@
 							<span class="name">Home</span>
 						</button>
 					</li>
-					<li>
+					<!--li>
 			        	<button id="menu-button" class="btn-tile" onclick="loadHelpOnce('/assets/php/get_modal.php?name=Test');">
 							<span class="glyphicon glyphicon-question-sign"></span>
 							<span class="name">Help</span>
 						</button>
-					</li>
+					</li-->
 				</ul>
 			</div>
 			<input type="file" id="load-input" accept=".ipar, .iparw" />
