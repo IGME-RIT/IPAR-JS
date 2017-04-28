2c17e982b0b529299ee192bd054d9259a67731f2
Noah Ratcliff <me@nratcliff.com>
2017-04-28 13:58:37 -0400

    update demo videos on about page

--- about.php
+++ about.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Forensics Project</title>
 		<?php include("assets/html/head.php"); ?>
+		<script src="/js/player.min.js"></script>
 	</head>
 	<body>
 		<?php include("assets/html/navbar.php"); ?>
@@ -29,10 +30,8 @@
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="yt-embed">
-                            <iframe class="yt-player" src="https://www.youtube.com/embed/Oq8GtXyUz1A" frameborder="0" allowfullscreen></iframe>
+                        <div class="embed-responsive embed-responsive-16by9 player" data-video-src="/assets/media/demo-full.mp4">
                         </div>
-                        
                     </div>
                 </div>
             </div>
@@ -45,8 +44,7 @@
                         <p>Developed alongside the game is an editor that can be used to generate new cases. Everything from subject matter to graphical elements to story can be set by the user to create an entertaining educational experience, and an reader that allows instructors to view the reports submitted by students for grading.</p>
                     </div>
                     <div class="col-md-4">
-                        <div class="yt-embed">
-                            <iframe class="yt-player" src="https://www.youtube.com/embed/i9FSdeNNRk8" frameborder="0" allowfullscreen></iframe>
+                        <div class="embed-responsive embed-responsive-16by9 player" data-video-src="/assets/media/editor-demo.mp4">
                         </div>
                     </div>
                 </div>
